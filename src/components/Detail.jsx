8d072f5e4f13e740ed9d57cfae291d8ca6adb464
carman12 <ryze1520@gmail.com>
2023-11-24 22:45:41 -0500

    subiendo el pi

---
+++ src/components/Detail.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState , useEffect } from "react";
+const APY_KEY = "pi-carman12";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+  useEffect(() => {
+    axios(
+      `https://rym2.up.railway.app/api/character/${id}?key=${APY_KEY}`
+    ).then(({ data }) => {
+      if (data.name) {
+        setCharacter(data);
+      } else {
+        window.alert("No hay personajes con ese ID");
+      }
+    });
+   setCharacter({});
+  }, [id]);
+
+  return  character.name ? (
+    <div>
+      <h1>Detalles del Personaje con ID: {id}</h1>
+         <h2>Nombre:{character.name}</h2>
+         <h2>Status:{character.status}</h2>
+         <h2>Especie:{character.species}</h2>
+         <h2>Genero:{character.gender}</h2>        
+         <h2>Origen:{character.origin?.name}</h2>        
+         <img src={character.image} alt='' /> 
+      </div>
+  ) : <h1>Cargando...</h1>
+};
+
+export default Detail;
